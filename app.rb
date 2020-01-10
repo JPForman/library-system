1d18ddac810f7e990d12497e6a7ff0eeb34432c8
Student <example@example.com>
2020-01-09 16:45:10 -0800

    update views, add routing

--- app.rb
+++ app.rb
@@ -34,90 +34,91 @@ get ('/books/:id') do
   erb(:book)
 end
 
-get ('/albums/:id/edit') do
-  @album = Album.find(params[:id].to_i())
-  erb(:edit_album)
+get ('/books/:id/edit') do
+  @book= Book.find(params[:id].to_i())
+  erb(:edit_book)
 end
 
-patch ('/albums/:id') do
-  @album = Album.find(params[:id].to_i())
-  @album.update(params[:name])
-  redirect to('/albums')
+patch ('/books/:id') do
+  @book = Book.find(params[:id].to_i())
+  @book.update(params[:name])
+  redirect to('/books')
 end
 
-delete ('/albums/:id') do
-  @album = Album.find(params[:id].to_i())
-  @album.delete()
-  redirect to('/albums')
+delete ('/books/:id') do
+  @book = Book.find(params[:id].to_i())
+  @book.delete()
+  redirect to('/books')
 end
 
-get ('/albums/:id/songs/:song_id') do
-  @song = Song.find(params[:song_id].to_i())
-  erb(:song)
+get ('/books/:id/authors/:author_id') do
+  @author = Author.find(params[:author_id].to_i())
+  erb(:author)
 end
 
-post ('/albums/:id/songs') do
-  @album = Album.find(params[:id].to_i())
-  song = Song.new({:name => params[:song_name], :album_id => @album.id, :id => nil})
-  song.save()
-  erb(:album)
+post ('/books/:id/authors') do
+  @book = Book.find(params[:id].to_i())
+  author = Author.new({:name => params[:author_name], :id => nil})
+  author.save()
+  author.update({:name => params[:author_name], :id => nil, :title => @book.name})
+  erb(:book)
 end
 
-patch ('/albums/:id/songs/:song_id') do
-  @album = Album.find(params[:id].to_i())
-  song = Song.find(params[:song_id].to_i())
-  song.update(params[:name], @album.id)
-  erb(:album)
+patch ('/books/:id/authors/:author_id') do
+  @book = Book.find(params[:id].to_i())
+  author = Author.find(params[:author_id].to_i())
+  author.update({:name => params[:author_name], :id => nil, :title => @book.name})
+  erb(:book)
 end
 
 delete ('/albums/:id/songs/:song_id') do
   song = Song.find(params[:song_id].to_i())
   song.delete
-  @album = Album.find(params[:id].to_i())
+  @book = Album.find(params[:id].to_i())
   erb(:album)
 end
 
-get('/artists') do
+get('/authors') do
   @artists = Artist.all
   erb(:artists)
 end
-
-get ('/artists/new') do
-  erb(:new_artist)
-end
-
-get ('/artists/:id') do
-  @artist = Artist.find(params[:id].to_i())
-  erb(:artist)
-end
-
-post ('/artists') do
-  artist = Artist.new({:name => params[:artist_name], :id => nil})
-  artist.save()
-  redirect to('/artists')
-end
-
-
-patch ('/artists/:id') do
-  @artist = Artist.find(params[:id].to_i())
-  @artist.update_name(params[:name])
-  redirect to('/artists')
-end
-
-delete ('/artists/:id') do
-  @artist = Artist.find(params[:id].to_i())
-  @artist.delete()
-  redirect to('/artists')
-end
-
-
-get ('/artists/:id/edit') do
-  @artist = Artist.find(params[:id].to_i())
-  erb(:edit_artist)
-end
-
-post ('/artists/:id/albums') do
-  @artist = Artist.find(params[:id].to_i())
-  @artist.update({:album_name => params[:album_name]})
-  erb(:artist)
-end
+#
+# get ('/artists/new') do
+#   erb(:new_artist)
+# end
+#
+# get ('/artists/:id') do
+#   @artist = Artist.find(params[:id].to_i())
+#   erb(:artist)
+# end
+#
+# post ('/artists') do
+#   artist = Artist.new({:name => params[:artist_name], :id => nil})
+#   artist.save()
+#   redirect to('/artists')
+# end
+#
+#
+# patch ('/artists/:id') do
+#   @artist = Artist.find(params[:id].to_i())
+#   @artist.update_name(params[:name])
+#   redirect to('/artists')
+# end
+#
+# delete ('/artists/:id') do
+#   @artist = Artist.find(params[:id].to_i())
+#   @artist.delete()
+#   redirect to('/artists')
+# end
+#
+#
+# get ('/artists/:id/edit') do
+#   @artist = Artist.find(params[:id].to_i())
+#   erb(:edit_artist)
+# end
+#
+# post ('/artists/:id/albums') do
+#   @artist = Artist.find(params[:id].to_i())
+#   @artist.update({:album_name => params[:album_name]})
+#   erb(:artist)
+# end
